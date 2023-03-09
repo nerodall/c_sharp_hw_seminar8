1ca0a66d06bb761e1200dd1ea5473c2b8c118dae
Alexey Neroda <neroda.all@mail.ru>
2023-03-09 21:52:18 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int[,] SortArray(int[,] array)
+{
+    for (int k = 0; k < array.Length; k++)
+    {
+
+        for (int i = 0; i < array.GetLength(0); i++)
+
+        {
+
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+
+        }
+    }
+    return array;
+}
+
+int[,] matrix = GenerateArray(4, 4);
+PrintArray(matrix);
+PrintArray(SortArray(matrix));
